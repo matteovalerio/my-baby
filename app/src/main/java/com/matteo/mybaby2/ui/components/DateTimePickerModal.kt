3d8e7a0f43c15f4128a8cd0a9a691529d5edcb58
matteovalerio <matteovalerio2006@hotmail.it>
2024-10-11 22:39:51 +0200

    feat: center content for DatePicker and DateTimePickerModal.kt

--- app/src/main/java/com/matteo/mybaby2/ui/components/DateTimePickerModal.kt
+++ app/src/main/java/com/matteo/mybaby2/ui/components/DateTimePickerModal.kt
@@ -1,3 +1,5 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
@@ -10,6 +12,7 @@ import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -60,10 +63,12 @@ fun DateTimePickerModal(
             }
         }
     ) {
-        if(isShowingDate.value) {
-            DatePicker(state = datePickerState)
-        } else {
-            TimePicker(state = timePickerState, modifier = Modifier.padding(16.dp))
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            if (isShowingDate.value) {
+                DatePicker(state = datePickerState)
+            } else {
+                TimePicker(state = timePickerState, modifier = Modifier.padding(16.dp))
+            }
         }
 
     }
